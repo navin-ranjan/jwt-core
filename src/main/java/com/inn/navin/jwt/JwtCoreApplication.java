6e92f94a03fd2e0df0035354440ae5c84c92cfff
navin-ranjan <navinranjan15498@gmail.com>
2023-08-14 23:11:13 +0530

    all file added

---
+++ src/main/java/com/inn/navin/jwt/JwtCoreApplication.java
@@ -0,0 +1,13 @@
+package com.inn.navin.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtCoreApplication.class, args);
+	}
+
+}
